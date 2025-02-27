bcf13204b9ca8160bb3ae3dc1103a6fe2d138800
Slade <saib6064@gmail.com>
2025-02-28 01:56:00 +0700

    feat(server): implemented RESTful API and connect to the database

--- server.js
+++ server.js
@@ -82,68 +82,77 @@ app.post("/detect-face", async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
     }
 })
-  
-
-
-const database = {
-    users : [
-        {
-            id: '123',
-            name: 'Slade',
-            email: 'gg@gmail.com',
-            password: 'cookies',
-            entries : 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sai',
-            email: 'ggez@gmail.com',
-            password: 'cookme',
-            entries : 0,
-            joined: new Date()
-        }
-    ],
-    login : [
-        {
-            id: '123',
-            hash: '',
-            email: 'gg@gmail.com'
-        }
-    ]
-} 
 
 app.get('/', (req,res) =>{
-    res.send(database.users);
+    res.json("Connected");
 })
 
-app.post('/signin' ,(req,res)=> {
+app.post('/signin' ,async (req,res)=> {
+    //email and password from frontend
     const {email , password} = req.body;
     
-    const user = database.users.find(user => user.email === email && user.password === password);
+    try {
+        //find hash first from login by using email
+        const [data] = await knex('login').select('email','hash').where({email:email});
+
+        if(!data){
+            res.status(400).json('User not found');
+        }
+
+        //compare hash
+        const isValid = bcrypt.compareSync(password, data.hash);
     
-    if(user){
-        res.json(user);
-    }else{
-        res.status(404).json('not found');
+        if(isValid){
+            //get user if password is correct
+            const [user] = await knex('users').select('*').where({email:email});
+            res.json(user);
+        }else{
+            //if the password is wrong
+            res.status(400).json('Wrong credentials');
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json('unable to get user');
     }
+    
 })
 
 app.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
-    try {
-        // Insert user data
-        const user = await knex('users').returning('*').insert({
-            email: email,
-            name: name,
-            joined: new Date()
-        });
-        // Send response
-        res.json(user[0]); // response contains inserted data, or just a success message
-    } catch (error) {
-        console.error(error);
-        res.status(500).json('Error inserting user');
-    }
+
+    //change password into hashcode
+    const hash = bcrypt.hashSync(password,14);
+
+    //we use trx when we have more than one operations
+    knex.transaction(async trx => {
+        try {
+
+            //add to login
+            const loginEmail = await trx('login').insert({
+                email : email,
+                hash: hash
+            }); 
+
+            //add to user
+            const [user] = await trx('users').insert({
+                email: email,
+                name:name,
+                joined: new Date()
+            }).returning('*');
+
+            // Commit transaction 
+            //if we don't commit, this process will never stop and Data won’t be saved.
+            await trx.commit();
+            res.json(user);
+
+        } catch (error) {
+            await trx.rollback(); // Rollback if there's an error and  Errors won’t undo changes.
+            console.error(error);
+            res.status(500).json('Transaction failed');
+        }        
+    })
+
 });
 
 //for future feature (not likely to happen tho lol)
@@ -162,23 +171,27 @@ app.get('/profile/:id', async(req,res) => {
     }
 })
 
-app.put('/image' , (req,res)=>{
+app.put('/image' , async(req,res)=>{
     const {id} = req.body;
 
-    const user = database.users.find(user => user.id === id);
+    try {
+        //get entries form users
+       const [ent] = await knex('users')
+       .where({ id: id })
+       .increment('entries', 1) //increment by 1 every time we call it
+       .returning('entries');
 
-    if(user){
-        user.entries++;
-        res.json(user.entries);
-    }else{
-        res.status(404).json('not found');
+       res.json(ent);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json('Error incrementing !!!');
     }
 })
 
 app.listen(port , ()=>{
     console.log("app is running on port " + port);
 })
-
 
 /*
 /--> res = this is working
