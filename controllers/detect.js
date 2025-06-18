b9b066bcb5f43986ded351099fd965e48f479325
Slade <saib6064@gmail.com>
2025-06-19 00:29:08 +0700

    feat(detect.js): use process.env.API_KEY for Clarifai PAT
    This change allows the Clarifai API key to be configured
    through an environment variable, improving security and
    flexibility.

--- controllers/detect.js
+++ controllers/detect.js
@@ -1,7 +1,7 @@
 //CLARIFAI API VERY IMPORTANT
 const returnClarifaiRequestOptions = function (img) {
   // Your PAT (Personal Access Token) can be found in the Account's Security section
-  const PAT = "bd65648ccafc4e2bad8173bb5feb02b4";
+  const PAT = process.env.API_KEY;
   // Specify the correct user_id/app_id pairings
   // Since you're making inferences outside your app's scope
   const USER_ID = "8rbi0vcpvzmz";
